a1ea51804c3105a3fc41b01609947d66f032103d
Mo9ata3a <terriblymekar@gmail.com>
2025-05-15 13:32:36 +0100

    Update AlternativeList.tsx

--- src/components/AlternativeList.tsx
+++ src/components/AlternativeList.tsx
@@ -35,7 +35,7 @@ const AlternativeList: React.FC<AlternativeListProps> = ({ alternatives }) => {
                 <span>{alt.categorie}</span>
                 <span className="font-medium">{alt.prix}€</span>
                 <div className="flex justify-between items-start">
-                <BanStatus status={alt.ban} />
+                <BanStatus status={true} />
           </div>
               </div>
             </div>
