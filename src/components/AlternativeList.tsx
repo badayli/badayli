a352fef71afef571f31f1a62eac904df6bd3b2c3
Mo9ata3a <terriblymekar@gmail.com>
2025-05-23 21:11:25 +0200

    Update AlternativeList.tsx

--- src/components/AlternativeList.tsx
+++ src/components/AlternativeList.tsx
@@ -31,7 +31,7 @@ const AlternativeList: React.FC<AlternativeListProps> = ({ alternatives }) => {
                   className="w-full h-full object-cover rounded"
                 />
               )}
-          </a> fffff  </div>
+          </a>   </div>
 
               
             <div className="min-w-0 flex-1">
